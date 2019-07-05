98ca365d1cc1cacab700e76cc6756f74835fa524
Shahbaz Saleem <shahbaz.saleem@emblemtechnologies.com>
2019-07-05 07:40:30 +0500

    updated color ext

--- CustomAlertController/BeAlertController.swift
+++ CustomAlertController/BeAlertController.swift
@@ -239,20 +239,14 @@ fileprivate extension UIColor {
             cString.remove(at: cString.startIndex)
         }
         
-        if ((cString.count) != 6) {
-            self.init(red: 255, green: 255, blue: 255, alpha: 1)
-            return
-        }else{
-            var rgbValue:UInt32 = 0
-            Scanner(string: cString).scanHexInt32(&rgbValue)
-            
-            self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-                      green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-                      blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-                      alpha: CGFloat(1.0)
-            )
-            return
-        }
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: CGFloat(1.0)
+        )
         
         
     }
